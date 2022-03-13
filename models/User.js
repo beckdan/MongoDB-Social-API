5858bdd4a4efe8afe13734e2c71bf8f26dc232c1
Dan Beck <danjacobbeck@gmail.com>
2022-03-13 00:02:39 -0800

    user.js done

--- models/User.js
+++ models/User.js
@@ -32,5 +32,13 @@ const userSchema = new Schema(
             virtuals: true,
           },
           id: false,
-        } 
-)
+        }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+  
+  const User = model('User', userSchema);
+  
+  module.exports = User;
