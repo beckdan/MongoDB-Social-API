57e0e48e331d374c9cfd49dfec083b8ef2afefc0
Dan Beck <danjacobbeck@gmail.com>
2022-03-13 00:20:51 -0800

    finished api routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const touter = require('express').Router();
+const { Router } = require('express');
+const {
+    createUser, 
+    updateUser,
+    getUsers, 
+    getUsersById,
+    deleteUser, 
+    addFriend, 
+    deleteFriend,
+    getUserById,
+} = require('../../controllers/usersController');
+
+router.route('/')
+.get(getUsers)
+.post(createUser)
+
+router.route('/:userId')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser)
+
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend)
+
+module.exports = router; 
