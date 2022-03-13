a0bbec75333dbc01f577207ef21fa1972abb6390
Dan Beck <danjacobbeck@gmail.com>
2022-03-12 23:41:03 -0800

    added get and delete users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,3 +19,36 @@ const createUser = (req, res) => {
       })
       .catch((err) => res.status(500).json(err));
   };
+
+  const getUsers = (req, res) => {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  };
+  
+  const getUserById = (req, res) => {
+    User.findOne({ _id: req.params.userId })
+      .then((user) => {
+        !user
+          ? res.status(404).json({ message: "No user found with that id" })
+          : res.json(user);
+      })
+      .catch((err) => res.status(500).json(err));
+  };
+
+  const deleteUser = async (req, res) => {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+  
+      if (!user) {
+        res.status(404).json({ message: "No user found with that ID" });
+      }
+  
+      await Thought.deleteMany({ userId: req.params.userId });
+  
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  };
