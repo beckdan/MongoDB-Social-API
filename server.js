40f914b29e1c8d1c303c43775006a6967eaea22f
Dan Beck <danjacobbeck@gmail.com>
2022-03-13 00:26:29 -0800

    added middleware

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API Server running on port ${PORT}`);
+  });
+});
